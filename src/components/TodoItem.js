2a4aa5fc4215fc532b43156802f70a7c228193c2
Sushant <sushant.adhikari.619@gmail.com>
2020-12-30 16:51:23 +0545

    file setup, installation of dependencies and components added

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                Hekko from the todo item 
+            </div>
+        )
+    }
+}
