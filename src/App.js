8cea55c689476c2f4a55b2a1d88a65fba1248294
Sushant <sushant.adhikari.619@gmail.com>
2020-12-30 20:25:18 +0545

    added state to the todo input

--- src/App.js
+++ src/App.js
@@ -3,17 +3,45 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import uuid from 'uuid';
+import {v4 as uuid} from 'uuid';
 
 class App extends Component {
+  state={
+    items: [],
+    id: uuid(),
+    item: '',
+    editItem: false
+  }
+  handleChange = (e) => {
+    this.setState({
+      item:e.target.value
+    });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      item: this.state.item
+    }
+
+    const updatedItems = [...this.state.items, newItem]
+
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuid(),
+      editedItem: false
+    })
+  };
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">todo input</h3>
-
-            <TodoInput />
+            <TodoInput item={this.state.item} handleChange={this.handleChange} 
+            handleSubmit={this.handleSubmit} />
         <TodoList />
           </div>
         </div>
